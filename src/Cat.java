6ab9e5c8f001da1e22356b8a55970b7d158f78a0
lilu1982 <homenetmedia82@gmail.com>
2021-08-24 14:00:41 +0200

    CAT angelegt

---
+++ src/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+
+    
+ 
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        System.out.printIn(cat);
+        
+    }
+    public static void ausgabe(String outputStr) {
+        
+    }
+}
