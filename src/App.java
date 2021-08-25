c35dd905ec9b609b036ae5972ba3f8ed68dc23e4
lilu1982 <homenetmedia82@gmail.com>
2021-08-25 07:58:41 +0200

    update

--- src/App.java
+++ src/App.java
@@ -1,12 +1,17 @@
 public class App {
     
-    public static void main(String[] args) {
-        ausgabe("Hi"); 
-    }
+  public static void main(String[] args) {
+      
+  Cat cat  = new Cat(); 
+  
+  ausgabe("Blick von aussen: " + cat);
+  cat.tellYourAddress();
+ 
+  }
 
-    public static void ausgabe(String outputStr) {
-      System.out.println(outputStr);  
-    }
+  public static void ausgabe(String outputStr) {
+    System.out.println(outputStr);  
+  }
 
 
 }
